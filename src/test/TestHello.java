50cd19f1d6c1db5293bc08af218a38ba8cb8cdad
Dineth Meemanage <dineth@rezgateway.com>
2017-03-27 13:13:42 +0530

    Test Commit 3

--- src/test/TestHello.java
+++ src/test/TestHello.java
@@ -1,7 +1,9 @@
+package test;
 public class TestHello{
 	public static void main(String[] args){
 		System.out.println("Hello Java");
 		System.out.println("Hello Dineth");
+		System.out.println("Test Java");
 	}
 
 }
